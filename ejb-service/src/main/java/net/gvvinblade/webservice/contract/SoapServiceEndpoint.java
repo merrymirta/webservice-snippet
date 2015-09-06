8ba74aac289759453a6cca40a6f087876531cc69
gvvinblade@gmail.com <845GjgRR#$)0>
2015-09-06 11:58:48 +0300

    changes

---
+++ ejb-service/src/main/java/net/gvvinblade/webservice/contract/SoapServiceEndpoint.java
@@ -0,0 +1,26 @@
+package net.gvvinblade.webservice.contract;
+
+import net.gvvinblade.webservice.domain.Person;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+/**
+ * Created by gvvin on 04.09.2015.
+ *
+ */
+@WebService(targetNamespace = "http://webservice.gvvinblade.net/personservice")
+@SOAPBinding(style = SOAPBinding.Style.DOCUMENT, use = SOAPBinding.Use.LITERAL,
+        parameterStyle = SOAPBinding.ParameterStyle.BARE)
+public interface SoapServiceEndpoint {
+
+    @WebMethod(operationName = "add", action="urn:add")
+    void add(@WebParam(name = "Person", partName = "person") Person person);
+
+    @WebMethod(operationName = "info", action="urn:info")
+    @WebResult(name = "Person", partName = "person") Person info(@WebParam(name = "name", partName = "name") String name);
+
+}
---
+++ ejb-service/src/main/java/net/gvvinblade/webservice/impl/PersonServiceImpl.java
@@ -0,0 +1,32 @@
+package net.gvvinblade.webservice.impl;
+
+import net.gvvinblade.webservice.contract.SoapServiceEndpoint;
+import net.gvvinblade.webservice.domain.Person;
+
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ *
+ * Created by gvvin on 04.09.2015.
+ */
+@Stateless
+@WebService(name = "PersonService", serviceName = "PersonService",
+        portName = "PersonServicePort",
+        endpointInterface = "net.gvvinblade.webservice.contract.SoapServiceEndpoint")
+public class PersonServiceImpl implements SoapServiceEndpoint {
+
+    private final Map<String, Person> personMap = new ConcurrentHashMap<>();
+
+    public void add(Person person) {
+        if (personMap.containsKey(person.getFirstName())) {
+            personMap.put(person.getFirstName(), person);
+        }
+    }
+
+    public Person info(String name) {
+        return personMap.get(name);
+    }
+}
